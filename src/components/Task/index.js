e9aaecaeb00bcf52db2a46bd8cbfd7731c59ff5a
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2023-03-27 18:04:14 +0300

    finishing all the logic correctle

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,7 +1,101 @@
-import React from 'react'
+// react
+import React, { useState } from "react";
 
-export default function Task() {
+// react-redux
+import { useDispatch } from "react-redux";
+
+// creator functions
+import { deleteTask } from "../../redux/reducers/TasksReducer/actions";
+
+import styles from "./styles.module.css";
+import { MdDeleteOutline } from "react-icons/md";
+import { FiEdit2 } from "react-icons/fi";
+import { TiTickOutline } from "react-icons/ti";
+
+export default function Task({ task, index }) {
+  const dispatch = useDispatch();
+  const [doneDivs, setDoneDivs] = useState([]);
+  const [isEditable, setIsEditable] = useState(false);
+  const [editableTask, setEditableTask] = useState({});
+  const [newTask, setNewTask] = useState("");
+
+  function handleDeleteTask(task) {
+    dispatch(deleteTask(task));
+  }
+
+  function handleDoneTask(index) {
+    setDoneDivs((prev) => [...prev, index]);
+  }
+
+  function handleEditTask(task) {
+    setIsEditable(true);
+
+    setEditableTask(task);
+  }
+
+  function handleSaveTask() {
+    setEditableTask((prev) => {
+      return {
+        ...prev,
+        taskDetails: newTask,
+      };
+    });
+
+    setIsEditable(false);
+  }
+
   return (
-    <div>Task</div>
-  )
+    <div
+      className={`${styles.taskCont} ${
+        doneDivs.includes(index) ? styles.doneTask : ""
+      }`}
+    >
+      <div>
+        {isEditable ? (
+          <div>
+            <input
+              onChange={(e) => setNewTask(e.target.value)}
+              type="text"
+              defaultValue={editableTask?.taskDetails}
+            />
+
+            <span>
+              <span onClick={handleSaveTask}>save</span>
+
+              <br />
+              <span onClick={() => setIsEditable(false)}>cancle</span>
+            </span>
+          </div>
+        ) : (
+          editableTask?.taskDetails || task?.taskDetails
+        )}
+      </div>
+
+      <div className={styles.taskControl}>
+        {/* Delete btn */}
+        <span>
+          <MdDeleteOutline
+            onClick={() => handleDeleteTask(task)}
+            className={styles.taskControlIcon}
+          />
+        </span>
+
+        {/* edit btn */}
+        <span>
+          <FiEdit2
+            onClick={() => handleEditTask(task)}
+            className={styles.taskControlIcon}
+          />
+        </span>
+
+        {/* done btn */}
+        <span>
+          <TiTickOutline
+            onClick={() => handleDoneTask(index)}
+            className={styles.taskControlIcon}
+          />
+        </span>
+      </div>
+    </div>
+  );
 }
--- src/pages/NewTask/index.js
+++ src/pages/NewTask/index.js
@@ -2,57 +2,23 @@
 import React, { useState } from "react";
 // react-redux
 import { useDispatch, useSelector } from "react-redux";
+// component
+import Task from "../../components/Task";
 
 // creator functions
-import {
-  addNewTask,
-  deleteTask,
-} from "../../redux/reducers/TasksReducer/actions";
+import { addNewTask } from "../../redux/reducers/TasksReducer/actions";
 
 // styles, icons
 import styles from "./styles.module.css";
 import { IoIosAdd } from "react-icons/io";
-import { MdDeleteOutline } from "react-icons/md";
-import { FiEdit2 } from "react-icons/fi";
-import { TiTickOutline } from "react-icons/ti";
 
 export default function NewTask() {
   const [inputData, setInputData] = useState("");
   const dispatch = useDispatch();
   const tasks = useSelector((state) => state.TasksReducer);
-  const [doneDivs, setDoneDivs] = useState([]);
-  const [isEditable, setIsEditable] = useState(false);
-  const [editableTask, setEditableTask] = useState({});
-  const [newTask, setNewTask] = useState("");
 
   function handleAddNewTask() {
     dispatch(addNewTask(inputData));
-  }
-
-  function handleDeleteTask(task) {
-    dispatch(deleteTask(task));
-  }
-
-  function handleDoneTask(index) {
-    setDoneDivs((prev) => [...prev, index]);
-  }
-
-  function handleEditTask(task) {
-    setIsEditable(true);
-
-    setEditableTask(task);
-  }
-  console.log(editableTask);
-
-  function handleSaveTask() {
-    setEditableTask((prev) => {
-      return {
-        ...prev,
-        taskDetails: newTask,
-      };
-    });
-
-    setIsEditable(false);
   }
 
   return (
@@ -79,59 +45,7 @@ export default function NewTask() {
 
       <div className={styles.tasksCont}>
         {tasks.map((task, index) => (
-          <div
-            key={index}
-            className={`${styles.taskCont} ${
-              doneDivs.includes(index) ? styles.doneTask : ""
-            }`}
-          >
-            <div>
-              {isEditable ? (
-                <div>
-                  <input
-                    onChange={(e) => setNewTask(e.target.value)}
-                    type="text"
-                    defaultValue={editableTask?.taskDetails}
-                  />
-
-                  <span>
-                    <span onClick={handleSaveTask}>save</span>
-
-                    <br />
-                    <span onClick={() => setIsEditable(false)}>cancle</span>
-                  </span>
-                </div>
-              ) : (
-                editableTask?.taskDetails || task?.taskDetails
-              )}
-            </div>
-
-            <div className={styles.taskControl}>
-              {/* Delete btn */}
-              <span>
-                <MdDeleteOutline
-                  onClick={() => handleDeleteTask(task)}
-                  className={styles.taskControlIcon}
-                />
-              </span>
-
-              {/* edit btn */}
-              <span>
-                <FiEdit2
-                  onClick={() => handleEditTask(task)}
-                  className={styles.taskControlIcon}
-                />
-              </span>
-
-              {/* done btn */}
-              <span>
-                <TiTickOutline
-                  onClick={() => handleDoneTask(index)}
-                  className={styles.taskControlIcon}
-                />
-              </span>
-            </div>
-          </div>
+          <Task key={index} task={task}/>
         ))}
       </div>
     </div>
